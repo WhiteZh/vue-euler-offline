00fe310545ea1d77e3fac0da7696dbc34bf74e05
John Bonjour <whitewolfzhang@gmail.com>
2026-01-03 11:54:01 +0800

    inlined favicon

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,15 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from '@/App.vue'
+import favIcon from "@/assets/favicon.png?inline";
+
+const iconLink = (() => {
+    const link = document.createElement('link');
+    link.rel = 'icon';
+    link.href = favIcon;
+
+    return link;
+})();
+document.head.appendChild(iconLink);
 
 createApp(App).mount('#app')
